9292fd093a05ae731635babb89aa1f573c4736b4
Vasanth Saravanan <vasanthsaravanancdm@gmail.com>
2022-11-04 21:30:04 +0530

    Updates

---
+++ precache-manifest.ac4c41039bff04a8cf3bb9db59255643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/trekup/static/css/main.6828a7ae.chunk.css"
+  },
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/trekup/static/js/main.cf8b110e.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/trekup/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "627298a29158292ca362",
+    "url": "/trekup/static/js/2.627298a2.chunk.js"
+  },
+  {
+    "revision": "88cf2b2b75f8dae31467",
+    "url": "/trekup/static/js/runtime~main.88cf2b2b.js"
+  },
+  {
+    "revision": "a8ffc05f60a3e4094937408e42505d1a",
+    "url": "/trekup/index.html"
+  }
+];
